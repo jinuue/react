1ff6d4e38953593ddc10de603559be0b2a5252b7
jinuue <66049165+jinuue@users.noreply.github.com>
2024-11-14 21:14:29 +0800

    Add files via upload

---
+++ sasa/src/data/tenantData.js
@@ -0,0 +1,32 @@
+const tenantData = [
+  {
+    tenant: 'John Doe',
+    room: 101,
+    bill: 30.5,
+    liters: 120,
+    maxLiters: 1000
+  },
+  {
+    tenant: 'Jane Doe',
+    room: 102,
+    bill: 50.75,
+    liters: 150,
+    maxLiters: 1000
+  },
+  {
+    tenant: 'Spongebob',
+    room: 103,
+    bill: 25.0,
+    liters: 80,
+    maxLiters: 1000
+  },
+  {
+    tenant: 'Patrick',
+    room: 104,
+    bill: 25.0,
+    liters: 80,
+    maxLiters: 1000
+  }
+];
+
+export default tenantData;
